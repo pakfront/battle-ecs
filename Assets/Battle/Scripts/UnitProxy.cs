a4e762acbff8130dc87741587c0ddd66e88de5a9
DESKTOP-C2SG422\Tim <tim@devnull.com>
2019-04-24 21:52:45 -0700

    Added simple battle tests

---
+++ Assets/Battle/Scripts/UnitProxy.cs
@@ -0,0 +1,22 @@
+using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Dux.Battle
+{
+    [RequiresEntityConversion]
+    public class UnitProxy : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        public float DegreesPerSecond = 360.0F;
+        
+        // The MonoBehaviour data is converted to ComponentData on the entity.
+        // We are specifically transforming from a good editor representation of the data (Represented in degrees)
+        // To a good runtime representation (Represented in radians)
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            var data = new UnitSpeed { RadiansPerSecond = math.radians(DegreesPerSecond) };
+            dstManager.AddComponentData(entity, data);
+        }
+    }
+}
---
+++ Assets/Battle/Scripts/UnitSpeed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Dux.Battle
+{
+    // Serializable attribute is for editor support.
+    [Serializable]
+    public struct UnitSpeed : IComponentData
+    {
+        public float RadiansPerSecond;
+    }
+}
+
