43da7bcc6744cd9d019a52b1d843bd8af46b1f15
Tim Speltz <>
2019-05-14 07:54:46 -0700

    still dont have subscenes working

--- Assets/ProxyTest/TestProxy.cs
+++ Assets/ProxyTest/TestProxy.cs
@@ -9,36 +9,37 @@ using Samples.HelloCube_02;
 
 namespace ProxyTest
 {
-        [RequiresEntityConversion]
-    public class TestProxy : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+    [RequiresEntityConversion]
+    public class TestProxy : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
     {
-        public GameObject rotationSpeedProxyPrefab;
-        // Start is called before the first frame update
+        
+        public Samples.HelloCube_02.RotationSpeedProxy rotationSpeedProxyPrefab;
+
+        // this is not needed, I think
        public void DeclareReferencedPrefabs(List<GameObject> gameObjects)
         {
-            gameObjects.Add(rotationSpeedProxyPrefab);
+            gameObjects.Add(rotationSpeedProxyPrefab.gameObject);
         }
         public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
         {
             dstManager.AddComponentData(entity, new TestComponentData { });
+            // SetName does not fail, but no name is seen in debugger
             dstManager.SetName(entity,"Parent");
 
-            //Entity prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(rotationSpeedProxyPrefab, dstManager.World);
+            Entity prefabEntity = conversionSystem.GetPrimaryEntity(rotationSpeedProxyPrefab.gameObject);
+            // I don't think instantiation of the prefab entity is needed
+            // dstManager.Instantiate(prefabEntity);
             int count = 4;
-
-            // NativeArray<Entity> kids = new NativeArray<Entity>(count, Allocator.Temp);
-            // dstManager.Instantiate(prefab, kids);
-            var kidPrimary = conversionSystem.GetPrimaryEntity(rotationSpeedProxyPrefab);
             for (int i = 0; i < count; i++)
             {
                 var kid = conversionSystem.CreateAdditionalEntity(rotationSpeedProxyPrefab);
+                // SetName does not fail, but no name is seen in debugger
                 dstManager.SetName(kid,"Kid"+i);
-                dstManager.SetComponentData(kid, new RotationSpeed { RadiansPerSecond  = 20*i });
+                // Error: ArgumentException: A component with type:RotationSpeed has not been added to the entity.
+                dstManager.SetComponentData(kid, new Samples.HelloCube_02.RotationSpeed { RadiansPerSecond  = 20*i });
             }
-            // kids.Dispose();
         }
     }
 
     [Serializable] public struct TestComponentData : IComponentData {}
-     
 }
