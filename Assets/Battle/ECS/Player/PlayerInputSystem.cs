dccba37ab8d5e6eb169b1ad110aaba75a40f4672
Tim Speltz <>
2019-05-16 22:12:24 -0700

    simplified PlayerInoutSystem to use IJobForEach

--- Assets/Battle/ECS/Player/PlayerInputSystem.cs
+++ Assets/Battle/ECS/Player/PlayerInputSystem.cs
@@ -14,19 +14,11 @@ namespace UnitAgent
     {
         private Plane groundplane = new Plane(Vector3.up, 0);
 
-        private EntityQuery m_Group;
         private EntityQuery m_PlayerSelectedNoGoal;
 
         protected override void OnCreate()
         {
-            m_Group = GetEntityQuery(
-               new EntityQueryDesc
-               {
-                   All = new ComponentType[] { typeof(GoalMoveTo), ComponentType.ReadOnly<PlayerSelected>() }
-               });
-
-            m_PlayerSelectedNoGoal = GetEntityQuery(
-               new EntityQueryDesc
+            m_PlayerSelectedNoGoal = GetEntityQuery( new EntityQueryDesc
                {
                    None = new ComponentType[] { typeof(GoalMoveTo) },
                    All = new ComponentType[] { ComponentType.ReadOnly<PlayerSelected>() }
@@ -34,41 +26,21 @@ namespace UnitAgent
         }
 
         [BurstCompile]
-        struct SetGoalJob : IJobChunk
+        [RequireComponentTag(typeof(PlayerSelected))]
+        struct SetGoalJob : IJobForEach<GoalMoveTo>
         {
-            public ArchetypeChunkComponentType<GoalMoveTo> GoalMoveToType;
             [ReadOnly] public float3 ClickLocation;
 
-            public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+            public void Execute(ref GoalMoveTo goalMoveTo)
             {
-                var goalMoveTo = chunk.GetNativeArray(GoalMoveToType);
-                for (var i = 0; i < chunk.Count; i++)
-                {
-                    // some computed offset from the click location
-                    goalMoveTo[i] =
-                        new GoalMoveTo { Position = ClickLocation + new float3(i, 0, chunkIndex) };
-                }
+                goalMoveTo.Position = ClickLocation; // + some offset
             }
         }
-
-        //  can burst compile
-        // [BurstCompile]
-        // [RequireComponentTag(typeof(PlayerSelected))]
-        // struct SetGoalJob : IJobForEach<GoalMoveTo>
-        // {
-        //     [ReadOnly] public float3 ClickLocation;
-
-        //     public void Execute(ref GoalMoveTo goalMoveTo)
-        //     {
-        //         goalMoveTo.Position = ClickLocation; // + some offset
-        //     }
-        // }
 
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
             if (!Input.GetMouseButtonDown(1)) return inputDeps;
 
-            //Create a ray from the Mouse click position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             float enter = 0.0f;
@@ -78,26 +50,14 @@ namespace UnitAgent
             Vector3 clickLocation = ray.GetPoint(enter);
             Debug.Log("PlayerInputSystem clickLocation "+clickLocation);
 
-            // var m_PlayerSelectedNoGoal = GetEntityQuery(
-            //    new EntityQueryDesc
-            //    {
-            //        None = new ComponentType[] { typeof(GoalMoveTo) },
-            //        All = new ComponentType[] { ComponentType.ReadOnly<PlayerSelected>() }
-            //    });
-
             EntityManager.AddComponent(m_PlayerSelectedNoGoal, typeof(GoalMoveTo));
 
-            // var job = new SetGoalJob
-            // {
-            //     ClickLocation = (float3)clickLocation
-            // };
             var job = new SetGoalJob
             {
-                ClickLocation = (float3)clickLocation,
-                GoalMoveToType = GetArchetypeChunkComponentType<GoalMoveTo>(false)
+                ClickLocation = (float3)clickLocation
             };
-
-            return job.Schedule(m_Group, inputDeps);
+ 
+            return job.Schedule(this, inputDeps);
         }
     }
 }
