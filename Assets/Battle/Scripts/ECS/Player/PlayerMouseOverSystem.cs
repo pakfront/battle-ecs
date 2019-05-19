d936b3d44dfc705835db2de80f157017f8f23a10
DESKTOP-C2SG422\Tim <tim@devnull.com>
2019-05-19 15:22:52 -0700

    moved cleanup system insto same file as main system

--- Assets/Battle/Scripts/ECS/Player/PlayerMouseOverSystem.cs
+++ Assets/Battle/Scripts/ECS/Player/PlayerMouseOverSystem.cs
@@ -111,4 +111,42 @@ namespace UnitAgent
             return outputDeps;
         }
     }
+
+    [UpdateInGroup(typeof(GameSystemGroup))]
+    [UpdateBefore(typeof(PlayerMouseOverSystem))]
+    public class ClearPlayerMouseOverSystem : ComponentSystem
+    {
+        private EntityQuery m_PlayerSelection, m_PlayerTarget, m_PlayerFollow;
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            m_PlayerSelection = GetEntityQuery(ComponentType.ReadOnly<PlayerSelection>());
+            m_PlayerTarget = GetEntityQuery(ComponentType.ReadOnly<PlayerTarget>());
+            m_PlayerFollow = GetEntityQuery(ComponentType.ReadOnly<PlayerFollow>());
+        }
+
+        protected override void OnUpdate()
+        {
+            // selection modifier pressed, early exit;
+            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) ||
+                 Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)
+                ) return;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Clearing PlayerSelection");
+                EntityManager.RemoveComponent(m_PlayerSelection, ComponentType.ReadOnly<PlayerSelection>());
+            }
+            else if (Input.GetKeyDown(KeyCode.T))
+            {
+                Debug.Log("Clearing PlayerTarget");
+                EntityManager.RemoveComponent(m_PlayerTarget, ComponentType.ReadOnly<PlayerTarget>());
+            }
+            else if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("Clearing PlayerFollow");
+                EntityManager.RemoveComponent(m_PlayerFollow, ComponentType.ReadOnly<PlayerFollow>());
+            }
+        }
+    }
 }
